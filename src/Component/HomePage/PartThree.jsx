d47dfc9266f26992d813a1d5d24b4cda48a0696d
itsrazib1 <itsrazib1@gmail.com>
2023-08-07 20:57:04 +0600

    first commit

---
+++ src/Component/HomePage/PartThree.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const PartThree = () => {
+    return (
+        <div>
+            <div className="hero min-h-screen border-2" style={{ backgroundImage: 'url(public/imgOne.jpg)' }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">PartThree</h1>
+                        {/* <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> */}
+                        {/* <button className="btn btn-primary">Get Started</button> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PartThree;
