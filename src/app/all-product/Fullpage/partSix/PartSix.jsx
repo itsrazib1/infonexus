9e724513679ca6909082b6359de834774ca46350
itsrazib1 <itsrazib1@gmail.com>
2023-08-21 14:28:09 +0600

    data added

--- src/app/all-product/Fullpage/partSix/PartSix.jsx
+++ src/app/all-product/Fullpage/partSix/PartSix.jsx
@@ -2,13 +2,13 @@
 import { UserData } from "@/app/data/Sales";
 import Image from "next/image";
 
-const UserCard = ({  username, firstName, email,image }) => (
+const UserCard = ({  name, discription, btn,image }) => (
     <div className="md:w-1/3 sm:w-full p-4 ">
         <div className="bg-white rounded-lg shadow-lg h-48 p-4">
             <Image width={100} height={100} src={image} alt="Image" className='h-10 border  w-10  ' />
-            <h2 className="text-xl font-semibold">{username}</h2>
-            <p className="text-gray-600">{`${firstName}`}</p>
-            <p className="text-blue-500 font-semibold ">{email}</p>
+            <h2 className="text-xl font-semibold">{name}</h2>
+            <p className="text-gray-600">{`${discription}`}</p>
+            <p className="text-blue-500 font-semibold ">{btn}</p>
         </div>
     </div>
 );
@@ -21,13 +21,13 @@ const PartSix = () => {
                 <div>Help your sales team be more productive with tools they love.</div>
             </div>
             <div className="flex flex-wrap ">
-                {UserData.map(({ id, username, firstName, email,image }) => (
+                {UserData.map(({ id, name, discription, btn,image }) => (
                     <UserCard
                         key={id}
                         id={id}
-                        username={username}
-                        firstName={firstName}
-                        email={email}
+                        name={name}
+                        discription={discription}
+                        btn={btn}
                         image={image}
                     />
                 ))}
--- src/app/data/Sales.js
+++ src/app/data/Sales.js
@@ -2,65 +2,65 @@ export const UserData = [
     {
       "id": 1,
       "image":"https://i.ibb.co/b7P0qDN/crm.png",
-      "username": "CRM",
-      "firstName": "Convert leads and close sales deals faster.",
-      "email": "TRY NOW"
+      "name": "CRM",
+      "discription": "Convert leads and close sales deals faster.",
+      "btn": "TRY NOW"
     },
     {
       "id": 2,
       "image":"https://i.ibb.co/L6CCD5g/images-q-tbn-ANd9-Gc-Q-F4jib-W-Pt-PMz3c-N27y-P07p-Ai-Dp-Uj-N6j4-w-usqp-CAU.png",
-      "username": "Forms",
-      "firstName": "Build online forms for every business need.",
-      "email": "TRY NOW"
+      "name": "Forms",
+      "discription": "Build online forms for every business need.",
+      "btn": "TRY NOW"
     },
     {
       "id": 3,
       "image":"https://i.ibb.co/BfL698x/images-q-tbn-ANd9-Gc-T-PIlqy9v-DBTRg-H35l-MWyv1yawwvi1-A7-Mu-AA-usqp-CAU.png",
-      "username": "SalesIQ",
-      "firstName": "Live-chat app to engage and convert website visitors.",
-      "email": "TRY NOW"
+      "name": "SalesIQ",
+      "discription": "Live-chat app to engage and convert website visitors.",
+      "btn": "TRY NOW"
     },
     {
       "id": 4,
       "image":"https://i.ibb.co/Gs1P94Y/calendar-337.png",
-      "username": "Bookings",
-      "firstName": "Appointment scheduling app for consultations with customers.",
-      "email": "TRY NOW"
+      "name": "Bookings",
+      "discription": "Appointment scheduling app for consultations with customers.",
+      "btn": "TRY NOW"
     },
     {
       "id": 5,
       "image":"https://i.ibb.co/sQNz8Vd/121-1212500-beaker-clipart-triangular-transparent-background-lab-beaker-icon.png",
-      "username": "Bigin",
-      "firstName": "Pipeline management solution for customer-facing teams.",
-      "email": "TRY NOW"
+      "name": "Bigin",
+      "discription": "Pipeline management solution for customer-facing teams.",
+      "btn": "TRY NOW"
     },
     {
       "id": 6,
       "image":"https://i.ibb.co/Wnfqrqj/66720.png",
-      "username": "CRM",
-      "firstName": "Convert leads and close sales deals faster.",
-      "email": "TRY NOW"
+      "name": "CRM",
+      "discription": "Convert leads and close sales deals faster.",
+      "btn": "TRY NOW"
     },
     {
       "id": 7,
       "image":"https://i.ibb.co/b7P0qDN/crm.png",
-      "username": "Sign",
-      "firstName": "Digital signature app for businesses.",
-      "email": "TRY NOW"
+      "name": "Sign",
+      "discription": "Digital signature app for businesses.",
+      "btn": "TRY NOW"
     },
     {
       "id": 8,
       "image":"https://i.ibb.co/3Rrnkty/location-icon-clipart-5z262.jpg",
-      "username": "RouteIQ",
-      "firstName": "Comprehensive sales map visualization and optimal route planning solution.",
-      "email": "TRY NOW"
+      "name": "RouteIQ",
+      "discription": "Comprehensive sales map visualization and optimal route planning solution.",
+      "btn": "TRY NOW"
     },
     {
       "id": 9,
       "image":"https://i.ibb.co/jW9mJmy/images-q-tbn-ANd9-Gc-Qu-H9-Qnm-Bu-r-Mxdu99-Ksqvgs8-Pb-Uv9rw5h-JJwpra-KOhi-U1jyw-Y5.png",
-      "username": "BETA Thrive",
-      "firstName": "A complete loyalty and affiliate management platform.",
-      "email": "TRY NOW"
+      "name": "BETA Thrive",
+      "discription": "A complete loyalty and affiliate management platform.",
+      "btn": "TRY NOW"
     }
   ]
   
