7d310928a97d977424599c5bafe79b3676ea730a
rezaul118255 <rezaul118255@gmail.com>
2023-08-22 20:24:03 +0600

    Json data compleate

---
+++ src/app/data/EmailCollaboration.js
@@ -0,0 +1,145 @@
+export const UserData = [
+    {
+        "id": 1,
+        "image": "https://i.ibb.co/93m53Vx/mail.png",
+        "name": "Mail",
+        "discription": "Secure email service for teams of all sizes.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 2,
+        "image": "https://i.ibb.co/GMj1yhx/Meeting.jpg",
+        "name": "Meeting",
+        "discription": "Run webinars online and conduct meetings securely.",
+        "btn": "TRY NOW"
+    },
+
+    {
+        "id": 3,
+        "image": "https://i.ibb.co/PCrJKCC/writer.png",
+        "name": "Writer",
+        "discription": "Word processor for focused writing and discussions. ",
+        "btn": "TRY NOW"
+    },
+
+    {
+        "id": 4,
+        "image": "https://i.ibb.co/0pN1hfs/sheet.png",
+        "name": "Sheet",
+        "discription": "The spreadsheet software for collaborative teams.",
+        "btn": "TRY NOW"
+    },
+
+    {
+        "id": 5,
+        "image": "https://i.ibb.co/Ptr6F5p/show.png",
+        "name": "Show",
+        "discription": "Create, edit, and share slides with a sleek presentation app.",
+        "btn": "TRY NOW"
+    },
+
+    {
+        "id": 6,
+        "image": "https://i.ibb.co/j8R4DW3/Notebook.png",
+        "name": "Notebook",
+        "discription": "A beautiful home for all your notes.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 7,
+        "image": "https://i.ibb.co/N35Pb3b/cliq.png",
+        "name": "Cliq",
+        "discription": "Stay in touch with teams no matter where you are.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 8,
+        "image": "https://i.ibb.co/vkpZhJK/connect.png",
+        "name": "Connect",
+        "discription": "Team collaboration software that brings people together.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 9,
+        "image": "https://i.ibb.co/thNzG6s/Bookings.png",
+        "name": "Bookings",
+        "discription": "Appointment scheduling app for consultations with customers.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 10,
+        "image": "https://i.ibb.co/JpFTsVJ/Lens.png",
+        "name": "TeamInbox",
+        "discription": "Shared inboxes for teams.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 11,
+        "image": "https://i.ibb.co/fGXtQXG/Subscriptions.png",
+        "name": "WorkDrive",
+        "discription": "Online file management for teams.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 12,
+        "image": "https://i.ibb.co/prZkw0Q/Sign.jpg",
+        "name": "Sign",
+        "discription": "Digital signature app for businesses.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 13,
+        "image": "https://i.ibb.co/vkpZhJK/connect.png",
+        "name": "Office Suite",
+        "discription": "Powerful collaborative work platform for teams.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 14,
+        "image": "https://i.ibb.co/JpFTsVJ/Lens.png",
+        "name": "Office Integrator",
+        "discription": "Built in document editors for web apps.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 15,
+        "image": "https://i.ibb.co/93m53Vx/mail.png",
+        "name": "ZeptoMail",
+        "discription": "Secure and reliable transactional email sending service.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 16,
+        "image": "https://i.ibb.co/V3yLcwk/Forms.png",
+        "name": "Calendar",
+        "discription": "Online business calendar to manage events and schedule appointments.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 18,
+        "image": "https://i.ibb.co/fGXtQXG/Subscriptions.png",
+        "name": "Learn",
+        "discription": "Knowledge and learning management platform.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 19,
+        "image": "https://i.ibb.co/2nknWZ0/Voice.png",
+        "name": "Voice",
+        "discription": "Cloud-based telephony software for businesses.",
+        "btn": "TRY NOW"
+    },
+    {
+        "id": 20,
+        "image": "https://i.ibb.co/PCrJKCC/writer.png",
+        "name": "ToDo",
+        "discription": "Collaborative task management for individuals and teams.",
+        "btn": "TRY NOW"
+    }
+
+
+
+
+
+
+]
