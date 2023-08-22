3ab01b78fb24f507b11917c7872dd976a6a5fd2f
AB-ohi <askohi63907@gmail.com>
2023-08-22 18:54:57 +0600

    added

--- src/services/DbConnect.js
+++ src/services/DbConnect.js
@@ -9,6 +9,7 @@ const DbConnect = async () => {
   if (db) return db;
   try {
     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@next-hero.ktnme97.mongodb.net/?retryWrites=true&w=majority`;
+    console.log(uri)
     // Create a MongoClient with a MongoClientOptions object to set the Stable API version
     const client = new MongoClient(uri, {
       serverApi: {
