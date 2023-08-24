d903e6996b03f000061bee57ef9e4f9c77dc307e
AB-ohi <askohi63907@gmail.com>
2023-08-24 13:30:26 +0600

    added

--- src/services/model/sales.js
+++ src/services/model/sales.js
@@ -7,3 +7,14 @@ const salesModel = new mongoose.Schema({
 });
 
 export const Sales = mongoose.models.sales || mongoose.model("sales", salesModel)
+
+
+// import mongoose from "mongoose";
+// const userModel = new mongoose.Schema({
+//   name: String,
+//   age: Number,
+//   city: String,
+//   email: String,
+// });
+
+// export const User = mongoose.models.user || mongoose.model("user", userModel)
