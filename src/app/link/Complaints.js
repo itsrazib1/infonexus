764d4bf77d6cc348782a54c26f7460c42e384288
anayet ullah <anayetdev@gmail.com>
2023-08-30 01:04:09 +0600

    Contact us, security, and complaint section done

--- src/app/link/Complaints.js
+++ src/app/link/Complaints.js
@@ -1,11 +1,135 @@
-
+import { FaRegCheckCircle } from "react-icons/fa";
 
 const Complaints = () => {
-    return (
-        <div className="h-screen bg-cyan-400-600" id="Security">
-            Complaints page
+  return (
+    <div id="Complaints">
+      <div className="md:p-16 p-5 bg-[#F1F3F4] border-2 border-t-0 border-[#dbdbdb]">
+        <h1 className="text-3xl font-bold text-center">IPR Complaints</h1>
+      </div>
+      <div className="md:w-2/3 mx-auto py-20 px-5">
+        <div className="">
+          <h2 className="text-3xl font-bold text-center mb-8">
+            IPR Complaints
+          </h2>
+          <p>
+            If you believe that content on any of the Zoho services has violated
+            your copyright or other intellectual property right, please write to
+            our Designated Agent providing the following information:
+          </p>
+          <ul className="my-5">
+            <li className="mb-3">
+              <FaRegCheckCircle className="inline-block text-green-500 text-xl" />{" "}
+              <span className="ml-2">An electronic or physical signature of the person authorized to
+              act on behalf of the owner of the copyright or other intellectual
+              property right alleged to be infringed;</span>
+            </li>
+            <li className="mb-3">
+              <FaRegCheckCircle className="inline-block text-green-500 text-xl" />{" "}
+              <span className="ml-2">A description of the copyrighted work or other intellectual
+              property that you claim to have been infringed;</span>
+            </li>
+            <li className="mb-3">
+              <FaRegCheckCircle className="inline text-green-500 text-xl" />{" "}
+             <span className="ml-2"> Identification of the material that you claim to be infringing
+              including information regarding its location that would enable it
+              to be located and if possible the URL of such infringing material;</span>
+            </li>
+            <li className="mb-3">
+              <FaRegCheckCircle className="inline text-green-500 text-xl" />{" "}
+              <span className="ml-2">
+                {" "}
+                Information that would enable us to contact you, such as your
+                address, telephone number, and email address (if available);
+              </span>
+            </li>
+            <li className="mb-3">
+              <FaRegCheckCircle className="inline text-green-500 text-xl" />{" "}
+              <span className="ml-2">
+                A statement by you to the effect that you believe in good faith
+                that the use of the infringing material in the manner complained
+                of is not authorized by the copyright or intellectual property
+                owner, its agent, or the law;
+              </span>
+            </li>
+            <li className="mb-3">
+              <FaRegCheckCircle className="inline text-green-500 text-xl"/>{" "}
+              <span className="ml-2">
+                A statement by you, under penalty of perjury, that the above
+                information contained in your communication is accurate and that
+                you are authorized to act on behalf of the owner of copyright or
+                other intellectual property right.
+              </span>
+            </li>
+          </ul>
         </div>
-    );
+        <div>
+          <p>
+            Your notice with the above information to be effective should be
+            sent to our Designated Agent for notice of claims of infringement of
+            copyright and intellectual property rights at the following address
+          </p>
+          <div className="my-8">
+            <p className="font-semibold">Designated Agent (IPR complaints)</p>
+            <p>C/o ZOHO Corporation</p>
+            <p>4141 Hacienda Drive</p>
+            <p>Pleasanton, CA 94588, USA</p>
+            <p className="text-blue-500 font-semibold">
+              Phone: +1-925-924-9500
+            </p>
+            <p>Fax: +1-925-924-9600</p>
+            <p className="text-blue-500 font-semibold">
+              Email: legal@zohocorp.com
+            </p>
+          </div>
+          <p>
+            Please take note that if you knowingly misrepresent that any
+            material or activity is infringing, you may be subject to liability
+            under Section 512(f) of the Copyright Act.
+          </p>
+        </div>
+
+        <div>
+          <h2 className="text-3xl font-bold text-center mt-24 mb-8">
+            Counter-notice Procedure
+          </h2>
+          <p>
+            If you are a subscriber and you feel that material posted by you on
+            any of the Zoho services has been wrongly removed, please write to
+            our Designated Agent providing the following information:
+          </p>
+        </div>
+        <div>
+          <p>
+            Your counter-notice with the above information to be effective
+            should be sent to our Designated Agent for notice of claims of
+            infringement of copyright and intellectual property rights at the
+            following address
+          </p>
+          <div className="my-8">
+            <p className="font-semibold">Designated Agent (IPR complaints)</p>
+            <p>C/o ZOHO Corporation</p>
+            <p>4141 Hacienda Drive</p>
+            <p>Pleasanton, CA 94588, USA</p>
+            <p className="text-blue-500 font-semibold">
+              Phone: +1-925-924-9500
+            </p>
+            <p>Fax: +1-925-924-9600</p>
+            <p className="text-blue-500 font-semibold">
+              Email: legal@zohocorp.com
+            </p>
+          </div>
+          <p>
+            Please take note that if you knowingly misrepresent that material
+            was removed or activity was disabled as a result of mistake or wrong
+            identification, you may be subject to liability under Section 512(f)
+            of the Copyright Acts. Also, kindly note that we have a policy of
+            terminating the accounts of subscribers who repeatedly infringe
+            copyright or other third party rights..
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Complaints;
--- src/app/link/Contact.js
+++ src/app/link/Contact.js
@@ -1,11 +1,406 @@
+import Image from "next/image";
+import cancel from "../../../public/icons/contact-page-icon/cancel.png";
+import help from "../../../public/icons/contact-page-icon/help.png";
+import renew from "../../../public/icons/contact-page-icon/renew.png";
+import product from "../../../public/icons/contact-page-icon/product.png";
+import usFlag from "../../../public/icons/contact-page-icon/us-flag.png";
+import argentina from "../../../public/icons/contact-page-icon/Argentina_Flag.png";
+import australia from "../../../public/icons/contact-page-icon/Flag_of_Australia.png";
+import bahrain from "../../../public/icons/contact-page-icon/Flag_of_Bahrain.png";
+import chile from "../../../public/icons/contact-page-icon/Flag_of_Chile.png";
+import china from "../../../public/icons/contact-page-icon/Flag_of_China.png";
+import colombia from "../../../public/icons/contact-page-icon/Flag_of_Colombia.png";
+import egypt from "../../../public/icons/contact-page-icon/Flag_of_Egypt.svg.png";
+import france from "../../../public/icons/contact-page-icon/Flag_of_France.png";
+import germany from "../../../public/icons/contact-page-icon/Flag_of_Germany.svg.png";
+import india from "../../../public/icons/contact-page-icon/Flag_of_India.png";
+import kenya from "../../../public/icons/contact-page-icon/Flag_of_Kenya.svg.png";
+import mexico from "../../../public/icons/contact-page-icon/Flag_of_Mexico.png";
+import nigeria from "../../../public/icons/contact-page-icon/Flag_of_Nigeria.png";
+import qatar from "../../../public/icons/contact-page-icon/Flag_of_Mexico.png";
+import southAfrica from "../../../public/icons/contact-page-icon/Flag_of_South_Africa.svg.png";
+import uk from "../../../public/icons/contact-page-icon/Flag_of_the_United_Arab_Emirates.svg.png";
+import italy from "../../../public/icons/contact-page-icon/italy-flag.png";
+import ksa from "../../../public/icons/contact-page-icon/saudi-flag.png";
+import japan from "../../../public/icons/contact-page-icon/2560px-Japan_flag_-_variant.png";
+import canada from "../../../public/icons/contact-page-icon/Flag_of_Canada.png";
+import uae from "../../../public/icons/contact-page-icon/Flag_of_the_United_Arab_Emirates.svg.png";
 
 
 const Contact = () => {
-    return (
-        <div className="h-screen bg-slate-600" id="Contact">
-            Contract Page
+  return (
+    <>
+      <div id="Contact" className="md:relative md:mb-40 mb-20">
+        <div className="bg-[#0060B4] text-white text-center md:h-[450px] md:relative py-5">
+          <h1 className="md:text-5xl text-3xl md:p-6 p-3">
+            Looking for something in particular?
+          </h1>
+          <p className="text-xl">We are here to help.</p>
+          <div className="accessed-app-header-line p-4 mt-8"></div>
         </div>
-    );
+        <div className="bg-white md:p-8 p-3 md:absolute  grid grid-cols-1 md:grid-cols-4 h-auto md:gap-0 gap-5  mx-auto w-10/12 border-solid shadow-xl mt-12 left-0 right-0 top-32  ">
+          <div className="card-side md:mr-5 border-2 bg-[#F6F9FD] border-[#e0eaf7]">
+            <div className="card-body">
+              <div className="w-10 ">
+                <figure>
+                  <Image className="" src={product} alt="Movie" />
+                </figure>
+              </div>
+              <p className="h-[120px]">
+                Looking for a demo, or have questions about our products and
+                pricing?
+              </p>
+              <a className="text-blue-400">sales@zohocorp.com</a>
+            </div>
+          </div>
+          <div className="card-side   bg-[#F6F9FD]  border-2 border-r-0 border-[#e0eaf7]">
+            <div className="card-body">
+              <div className="w-10 ">
+                <figure>
+                  <Image className="" src={help} alt="Movie" />
+                </figure>
+              </div>
+              <p className="h-[120px]">
+                Need help with adding licenses, changing plans, or additional
+                services?
+              </p>
+              <a className=" text-blue-400">upgrade@zohocorp.com</a>
+            </div>
+          </div>
+          <div className="card-side  bg-[#F6F9FD] border-2 border-[#e0eaf7]">
+            <div className="card-body">
+              <div className="w-10 ">
+                <figure>
+                  <Image className="" src={renew} alt="Movie" />
+                </figure>
+              </div>
+              <p className="h-[120px]">
+                Want to renew your subscription or need details about your
+                current plan?
+              </p>
+              <a className=" text-blue-400">renewal@zohocorp.com</a>
+            </div>
+          </div>
+          <div className="card-side   bg-[#F6F9FD] border-2 border-l-0 border-[#e0eaf7]">
+            <div className="card-body">
+              <div className="w-10 ">
+                <figure>
+                  <Image className="" src={cancel} alt="Movie" />
+                </figure>
+              </div>
+              <p className="h-[120px]">
+                Need to cancel or downgrade your subscription? We are sad to see
+                you go, but ready to help.
+              </p>
+              <a className=" text-blue-400">cancellation@zohocorp.com</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#F6F9FD] md:py-16 py-8">
+        <div className="md:flex md:gap-8 md:w-2/3 mx-auto justify-between">
+          <div className="md:w-2/5 p-8">
+            <p className="text-green-600">
+              <small className="uppercase">I am new here</small>
+            </p>
+            <h2 className="text-3xl my-3">
+              Interested in a product? Talk to our sales team
+            </h2>
+            <p>
+              From questions about pricing to one-on-one personalized demos, we
+              would love to connect and help get you started.
+            </p>
+          </div>
+          <div className="md:w-3/5 items-center md:p-8 p-4">
+            <div className="flex gap-3 bg-white p-4">
+              <div>
+                <Image className="w-8" src={usFlag} alt="" />
+              </div>
+              <div>
+                <div className="text-blue-500">
+                  <p className="font-semibold text-lg text-black">US</p>
+                  <p>+1 877 834 4428</p>
+                  <p>+1 844 584 2497</p>
+                  <p>+1 833 968 1705</p>
+                  <p>+1(888) 900 9646 (Support)</p>
+                  <p>sales@zohocorp.com</p>
+                </div>
+              </div>
+            </div>
+            <div className="join join-vertical w-full mt-3  bg-white p-8">
+              <div className="collapse collapse-arrow join-item border-b-[1px] border-base-300">
+                <input type="radio" name="my-accordion-4" checked="checked" />
+                <div className="collapse-title text-xl font-medium text-base-content">
+                  Canada
+                </div>
+                <div className="collapse-content">
+                  <div className="flex gap-3">
+                    <div>
+                      <Image className="w-8" src={canada} alt="" />
+                    </div>
+                    <div className="text-blue-500">
+                      <p className="font-semibold text-lg text-black">US</p>
+                      <p>+1 877 834 4428</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="collapse collapse-arrow join-item border-b-[1px] border-base-300">
+                <input type="radio" name="my-accordion-4" />
+                <div className="collapse-title text-xl font-medium text-base-content">
+                  Latin America
+                </div>
+                <div className="collapse-content">
+                  <div className="md:flex justify-between">
+                    <div>
+                      <div className="flex gap-3 mb-3">
+                        <div>
+                          <Image className="w-8" src={argentina} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Argentina</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3">
+                        <div>
+                          <Image className="w-8" src={chile} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Chile</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex gap-3 mb-3">
+                        <div>
+                          <Image className="w-8" src={colombia} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Colombia</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3">
+                        <div>
+                          <Image className="w-8" src={mexico} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Mexico</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="collapse collapse-arrow join-item border-b-[1px] border-base-300">
+                <input type="radio" name="my-accordion-4" />
+                <div className="collapse-title text-xl font-medium text-base-content ">
+                  Asia pacific
+                </div>
+                <div className="collapse-content">
+                  <div className="md:flex justify-between">
+                    <div>
+                      <div className="flex gap-3">
+                        <div>
+                          <Image className="w-8" src={india} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">India</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3 mt-3">
+                        <div>
+                          <Image className="w-8" src={japan} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Japan</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex gap-3">
+                        <div>
+                          <Image className="w-8" src={australia} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Australia</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3 mt-3">
+                        <div>
+                          <Image className="w-8" src={china} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">China</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="collapse collapse-arrow join-item border-b-[1px] border-base-300">
+                <input type="radio" name="my-accordion-4" />
+                <div className="collapse-title text-xl font-medium text-base-content">
+                  Middle East
+                </div>
+                <div className="collapse-content">
+                  <div className="md:flex justify-between">
+                    <div>
+                      <div className="flex gap-3">
+                        <div>
+                          <Image className="w-8" src={ksa} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">KSA</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3 mt-3">
+                        <div>
+                          <Image className="w-8" src={qatar} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Qatar</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex gap-3">
+                        <div>
+                          <Image className="w-8" src={uae} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">UAE</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3">
+                        <div>
+                          <Image className="w-8" src={bahrain} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Bahrain</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="collapse collapse-arrow join-item border-b-[1px] border-base-300">
+                <input type="radio" name="my-accordion-4" />
+                <div className="collapse-title text-xl font-medium text-base-content">
+                  Africa
+                </div>
+                <div className="collapse-content">
+                  <div className="md:flex justify-between">
+                    <div>
+                      <div className="flex gap-3">
+                        <div>
+                          <Image className="w-8" src={southAfrica} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">South Africa</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3 mt-3">
+                        <div>
+                          <Image className="w-8" src={nigeria} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Nigeria</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex gap-3 mb-3">
+                        <div>
+                          <Image className="w-8" src={egypt} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Egypt</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3 mt-3">
+                        <div>
+                          <Image className="w-8" src={kenya} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Kenya</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="collapse collapse-arrow join-item border-b-[1px] border-base-300">
+                <input type="radio" name="my-accordion-4" />
+                <div className="collapse-title text-xl font-medium text-base-content">
+                  Europe
+                </div>
+                <div className="collapse-content">
+                  <div className="md:flex justify-between">
+                    <div>
+                      <div className="flex gap-3">
+                        <div>
+                          <Image className="w-8" src={france} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">France</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3 mt-3">
+                        <div>
+                          <Image className="w-8" src={uk} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">UK</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex gap-3">
+                        <div>
+                          <Image className="w-8" src={germany} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Germany</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3 mt-3">
+                        <div>
+                          <Image className="w-8" src={italy} alt="" />
+                        </div>
+                        <div className="text-blue-500">
+                          <p className="font-semibold text-lg text-black">Italy</p>
+                          <p>+1 877 834 4428</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
