4c4a9d3599958dd323caa77c7eb473368a1d10b7
itsrazib1 <itsrazib1@gmail.com>
2023-08-31 16:29:55 +0600

    a

--- src/app/all-product/Fullpage/partSix/Sales.jsx
+++ src/app/all-product/Fullpage/partSix/Sales.jsx
@@ -1,16 +1,23 @@
 
-
-import Image from "next/image";
-import UserCard from "./UserCard/UserCard";
 import getSales from "@/app/utils/getsales";
+import Image from "next/image";
 
 
 
-
+const UserCard = ({  name, discription, btn,image }) => (
+    <div className="md:w-1/3 sm:w-full p-4 ">
+        <div className="bg-white rounded-lg shadow-lg h-48 p-4">
+            <Image width={100} height={100} src={image} alt="Image" className='h-10 border  w-10  ' />
+            <h2 className="text-xl font-semibold">{name}</h2>
+            <p className="text-gray-600">{`${discription}`}</p>
+            <p className="text-blue-500 font-semibold ">{btn}</p>
+        </div>
+    </div>
+);
 const Sales = async () => {
     
 const sales = await getSales();
-
+// console.log(sales);
     return (
         <div id="sales" className="container mx-auto p-8">
             <div>
@@ -18,12 +25,12 @@ const sales = await getSales();
                 <div>Help your sales team be more productive with tools they love.</div>
             </div>
             <div className="flex flex-wrap ">
-                {sales.map(({ _id, name, description, btn,image }) => (
+                {sales.map(({ id, name, discription, btn,image }) => (
                     <UserCard
-                        key={_id}
-                        id={_id}
+                        key={id}
+                        id={id}
                         name={name}
-                        description={description}
+                        discription={discription}
                         btn={btn}
                         image={image}
                     />
