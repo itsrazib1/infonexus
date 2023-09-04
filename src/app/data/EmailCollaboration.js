a56fd92e3df30606315e18d730eb3a697873ff6d
rezaul118255 <rezaul118255@gmail.com>
2023-09-04 14:31:26 +0600

    Json data update

--- src/app/data/EmailCollaboration.js
+++ src/app/data/EmailCollaboration.js
@@ -3,12 +3,14 @@ export const EmailCollaborationData = [
         "image": "https://i.ibb.co/93m53Vx/mail.png",
         "name": "Mail",
         "description": "Secure email service for teams of all sizes.",
+        "services": "Zoho Mail is your all-in-one email solution designed for teams of all sizes. It offers a secure and feature-rich platform for efficient communication within your organization. With robust email management tools, intuitive interfaces, and advanced security features, Zoho Mail ensures that your team's email experience is smooth and productive.Whether you're a small business or a large enterprise, Zoho Mail adapts to your needs. It provides seamless integration with other Zoho apps and third-party services, making it a versatile choice for email collaboration. Try Zoho Mail now to experience a secure and efficient email service that enhances teamwork.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/GMj1yhx/Meeting.jpg",
         "name": "Meeting",
         "description": "Run webinars online and conduct meetings securely.",
+        "services": "Zoho Meeting is your comprehensive solution for hosting webinars and conducting online meetings securely. It simplifies remote collaboration, enabling you to connect with teams and clients worldwide. With features like screen sharing, interactive whiteboards, and secure access controls, Zoho Meeting ensures productive and engaging online interactions.Whether you're conducting training sessions, client presentations, or internal meetings, Zoho Meeting provides a user-friendly platform. It supports a variety of devices and browsers, making it accessible to a wide audience. Try Zoho Meeting now to take your virtual meetings and webinars to the next level.",
         "btn": "TRY NOW"
     },
 
@@ -16,6 +18,7 @@ export const EmailCollaborationData = [
         "image": "https://i.ibb.co/PCrJKCC/writer.png",
         "name": "Writer",
         "description": "Word processor for focused writing and discussions. ",
+        "services": "Zoho Writer is a powerful word processing tool designed for focused writing and collaborative discussions. It offers a distraction-free writing environment with advanced formatting options and real-time collaboration features.With Zoho Writer, teams can work together on documents, leave comments, and track changes seamlessly. It's an ideal choice for businesses and professionals looking to streamline document creation and collaboration. Try Zoho Writer now to simplify your writing and document collaboration processes.",
         "btn": "TRY NOW"
     },
 
@@ -23,6 +26,7 @@ export const EmailCollaborationData = [
         "image": "https://i.ibb.co/0pN1hfs/sheet.png",
         "name": "Sheet",
         "description": "The spreadsheet software for collaborative teams.",
+        "services": "Zoho Sheet is the ultimate spreadsheet software for collaborative teams. It simplifies data analysis and allows multiple users to work on spreadsheets simultaneously. With features like real-time editing, formula calculations, and data visualization tools, Zoho Sheet empowers teams to make data-driven decisions more efficiently. Whether you're managing financial data, project plans, or inventory lists, Zoho Sheet offers the flexibility and scalability needed for effective spreadsheet collaboration. Try Zoho Sheet now to enhance your team's productivity and data management capabilities.",
         "btn": "TRY NOW"
     },
 
@@ -30,6 +34,7 @@ export const EmailCollaborationData = [
         "image": "https://i.ibb.co/Ptr6F5p/show.png",
         "name": "Show",
         "description": "Create, edit, and share slides with a sleek presentation app.",
+        "services": "Zoho Show is your sleek presentation app that simplifies the creation, editing, and sharing of slides. It offers a user-friendly interface with powerful features for designing stunning presentations. With Zoho Show, you can collaborate with team members in real-time, access a library of templates and visuals, and deliver impactful presentations. Whether you're in a boardroom or a virtual meeting, Zoho Show helps you captivate your audience. Try Zoho Show now to create presentations that leave a lasting impression",
         "btn": "TRY NOW"
     },
 
@@ -37,84 +42,98 @@ export const EmailCollaborationData = [
         "image": "https://i.ibb.co/j8R4DW3/Notebook.png",
         "name": "Notebook",
         "description": "A beautiful home for all your notes.",
+        "services": "Zoho Notebook provides a beautiful and organized space for all your notes, ideas, and information. It's the perfect digital notebook for individuals and teams looking to centralize their notes and collaborate effectively.With Zoho Notebook, you can create notebooks, jot down notes, attach files, and share content with colleagues. It synchronizes seamlessly across devices, ensuring that you have access to your notes whenever and wherever you need them. Try Zoho Notebook now to keep your ideas and information organized in a visually appealing way.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/N35Pb3b/cliq.png",
         "name": "Cliq",
         "description": "Stay in touch with teams no matter where you are.",
+        "services": "Zoho Cliq is a team communication and collaboration platform designed to keep teams connected, no matter where they are. It offers chat, video calls, and file sharing features to streamline team communication.With Zoho Cliq, you can create channels for specific projects, departments, or topics, making it easy to organize conversations. It also integrates with other Zoho apps and third-party tools, enhancing its versatility. Try Zoho Cliq now to stay connected and collaborate effectively with your team.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/vkpZhJK/connect.png",
         "name": "Connect",
         "description": "Team collaboration software that brings people together.",
+        "services": "Zoho Connect is a team collaboration software that brings people together, fostering seamless communication and collaboration within organizations. It offers a centralized platform for teams to connect, share, and work together efficiently.With features like feeds, groups, and task management, Zoho Connect simplifies team interactions and project management. It's a valuable tool for businesses looking to enhance teamwork and productivity. Try Zoho Connect now to create a cohesive and collaborative work environment.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/thNzG6s/Bookings.png",
         "name": "Bookings",
         "description": "Appointment scheduling app for consultations with customers.",
+        "services": "Zoho Bookings is an appointment scheduling app designed for consultations with customers. It simplifies the process of booking appointments, managing schedules, and offering clients a convenient way to connect with your business. With Zoho Bookings, you can customize your booking pages, set up automated reminders, and synchronize appointments with your calendar. It's the ideal solution for service-based businesses seeking to streamline their appointment management. Try Zoho Bookings now to offer hassle-free scheduling to your clients.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/JpFTsVJ/Lens.png",
         "name": "TeamInbox",
         "description": "Shared inboxes for teams.",
+        "services": "Zoho TeamInbox provides shared inboxes for teams, streamlining email communication and collaboration. It offers a centralized platform where teams can manage emails, assign tasks, and collaborate on responses. With features like email categorization, tags, and real-time collaboration, Zoho TeamInbox ensures that email conversations are organized and productive. It's a valuable tool for teams looking to improve email management and customer support. Try Zoho TeamInbox now to enhance your team's email collaboration.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/fGXtQXG/Subscriptions.png",
         "name": "WorkDrive",
         "description": "Online file management for teams.",
+        "services": "Zoho WorkDrive is an online file management solution designed for teams. It offers a secure and collaborative platform for storing, sharing, and organizing files and documents.With features like file versioning, real-time editing, and team folders, Zoho WorkDrive simplifies document collaboration and ensures data security. It's an ideal choice for businesses looking to streamline their file management processes. Try Zoho WorkDrive now to take control of your team's files and documents. ",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/prZkw0Q/Sign.jpg",
         "name": "Sign",
         "description": "Digital signature app for businesses.",
+        "services": "Zoho Sign is a digital signature app tailored for businesses. It simplifies the process of signing and sending documents securely, eliminating the need for paper-based signatures.With features like e-signature workflows, document tracking, and integration capabilities, Zoho Sign offers a streamlined solution for contract management and approvals. It's an essential tool for businesses seeking to digitize their signature processes and improve document efficiency. Try Zoho Sign now to experience hassle-free digital signatures.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/vkpZhJK/connect.png",
         "name": "Office Suite",
         "description": "Powerful collaborative work platform for teams.",
+        "services": "Zoho Office Suite is a powerful collaborative work platform designed for teams. It provides a suite of office productivity tools, including word processing, spreadsheets, and presentations, all within a single integrated platform.With real-time collaboration, cloud storage, and offline access, Zoho Office Suite empowers teams to work together seamlessly on documents and projects. It's the perfect solution for businesses looking to enhance their productivity and collaboration. Try Zoho Office Suite now to transform the way your team works",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/JpFTsVJ/Lens.png",
         "name": "Office Integrator",
         "description": "Built in document editors for web apps.",
+        "services": "Zoho Office Integrator provides built-in document editors for web apps, making it easy to create and edit documents within your web applications. It offers seamless integration with Zoho's suite of office productivity tools.With Zoho Office Integrator, you can embed document editors into your web applications, allowing users to work on documents without leaving your application. It's a valuable tool for web developers and businesses seeking to enhance document collaboration within their applications. Try Zoho Office Integrator now to provide a seamless document editing experience.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/93m53Vx/mail.png",
         "name": "ZeptoMail",
         "description": "Secure and reliable transactional email sending service.",
+        "services": "Zoho ZeptoMail is a secure and reliable transactional email sending service. It's designed to ensure that your transactional emails, such as order confirmations and account notifications, are delivered efficiently and securely.With features like email templates, tracking, and analytics, Zoho ZeptoMail simplifies transactional email management. It's a crucial tool for businesses looking to enhance their customer communication through transactional emails. Try Zoho ZeptoMail now to ensure the reliable delivery of your transactional emails.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/V3yLcwk/Forms.png",
         "name": "Calendar",
         "description": "Online business calendar to manage events and schedule appointments.",
+        "services": "Zoho Calendar is an online business calendar designed to manage events and schedule appointments efficiently. It offers a user-friendly interface and a set of features to help you stay organized and on top of your schedule. With Zoho Calendar, you can create events, set reminders, and share calendars with team members or clients. It's an essential tool for businesses and professionals seeking to manage their time effectively. Try Zoho Calendar now to simplify event management and scheduling.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/fGXtQXG/Subscriptions.png",
         "name": "Learn",
         "description": "Knowledge and learning management platform.",
+        "services": "Zoho Learn is a knowledge and learning management platform designed to facilitate training and education within organizations. It offers a centralized platform for creating, managing, and delivering learning content. With features like course creation, assessments, and learner tracking, Zoho Learn simplifies the process of training employees and clients. It's a valuable tool for businesses looking to enhance their learning and development programs. Try Zoho Learn now to create engaging and effective training experiences.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/2nknWZ0/Voice.png",
         "name": "Voice",
         "description": "Cloud-based telephony software for businesses.",
+        "services": "Zoho Voice is a cloud-based telephony software designed for businesses. It offers a comprehensive suite of telephony features, including call routing, voicemail, and call analytics. With Zoho Voice, you can streamline your business communications and provide exceptional customer support over the phone. It's an essential tool for businesses seeking to improve their telephony capabilities and enhance customer interactions. Try Zoho Voice now to elevate your business communications",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/PCrJKCC/writer.png",
         "name": "ToDo",
         "description": "Collaborative task management for individuals and teams.",
+        "services": "Zoho ToDo is a collaborative task management tool designed for individuals and teams. It simplifies task creation, assignment, and tracking, ensuring that teams stay organized and productive.With features like task lists, deadlines, and task assignments, Zoho ToDo empowers teams to manage their projects effectively. It's a valuable tool for businesses looking to improve task management and teamwork. Try Zoho ToDo now to enhance your team's task management processes.",
         "btn": "TRY NOW"
     }
 
--- src/app/data/Marketing.js
+++ src/app/data/Marketing.js
@@ -3,84 +3,98 @@ export const MarketingData = [
         "image": "https://i.ibb.co/sQzCfy8/Zoho-Social.png",
         "name": "Social",
         "description": "The all-in-one social media management software.",
+        "services": "Zoho Social is your all-in-one social media management software. It's the key to streamlining your social media efforts effectively. With Zoho Social, you can schedule and publish posts, engage with your audience, and analyze your social media performance all from one platform.Whether you're managing multiple social media accounts or looking to enhance your social media strategy, Zoho Social has you covered. It offers valuable insights to help you make data-driven decisions and grow your online presence. Try Zoho Social now to supercharge your social media marketing.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/thBs64f/campaigns.png",
         "name": "Campaigns",
         "description": "Create, send, and track targeted email campaigns that drive sales.",
+        "services": "Zoho Campaigns is your go-to solution for creating, sending, and tracking targeted email campaigns that can significantly impact your sales. Email marketing remains a powerful tool for reaching your audience, and Zoho Campaigns makes it easy to design eye-catching emails, segment your contacts, and analyze campaign results.Whether you're a small business or a large enterprise, Zoho Campaigns can help you build stronger customer relationships and drive conversions. Try Zoho Campaigns now and take your email marketing to the next level",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/V3yLcwk/Forms.png",
         "name": "Forms",
         "description": "Build online forms for every business need.",
+        "services": "Zoho Forms is your versatile tool for building online forms that cater to all your business needs. Whether you require simple contact forms, detailed surveys, or event registrations, Zoho Forms offers a user-friendly interface to create customized forms effortlessly.With Zoho Forms, you can collect valuable data, automate workflows, and gain insights from real-time analytics. It's an ideal solution for businesses looking to streamline data collection processes. Try Zoho Forms now and revolutionize the way you gather and manage information.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/RQZvqPd/Survey.png",
         "name": "Survey",
         "description": "Design surveys to reach and interact with your audience.",
+        "services": "Designing surveys to reach and interact with your audience has never been easier than with Zoho Survey. It's a comprehensive platform that empowers you to create engaging surveys, collect responses, and gain actionable insights.Whether you're conducting market research or seeking feedback from your customers, Zoho Survey offers a range of features to meet your needs. It's a powerful tool for businesses of all sizes to make informed decisions based on valuable data. Try Zoho Survey now and elevate your understanding of your audience.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/PDcQN7c/Sites.png",
         "name": "Sites",
         "description": "Online website builder with extensive customisation options.",
+        "services": "Zoho Sites is your online website builder with extensive customization options. Create a stunning online presence for your business or personal projects without the need for coding skills.With a drag-and-drop interface and a variety of templates, Zoho Sites makes web design a breeze. Whether you're launching a blog, portfolio, or business website, you can do it all with Zoho Sites. Try it now to bring your online vision to life",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/p0NP82B/Page-Sense.png",
         "name": "PageSense",
         "description": "Website conversion optimization and personalisation platform.",
+        "services": "Zoho PageSense is the ultimate tool for website conversion optimization and personalization. It's your key to understanding user behavior and making informed decisions to boost your website's conversion rates.With PageSense, you can perform A/B testing to compare different versions of your webpages, create heatmaps to visualize user interactions, and set up funnel analysis to track the user journey on your site. These insights empower you to make targeted changes that lead to higher engagement and conversions.Whether you run an e-commerce store, a blog, or any other type of website, PageSense helps you fine-tune your online presence for maximum impact. If you're serious about optimizing your website's performance, give Zoho PageSense a try now",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/QHD67Rc/Webinar.png",
         "name": "Webinar",
         "description": "Webinar platform for webcasting online webinars.",
+        "services": "Zoho Webinar is your go-to platform for hosting online webinars. Whether you're planning product launches, educational sessions, or team meetings, this platform simplifies the entire process.You can create registration pages to manage sign-ups, engage with your audience through live chat, and gather valuable insights through comprehensive analytics. Zoho Webinar ensures a smooth and interactive experience for both hosts and attendees.If you want to connect and engage with your audience effectively through webinars, Zoho Webinar is the solution you need. Try it now and take your online events to the next level.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/sCK3bzH/Commerce.png",
         "name": "Commerce",
         "description": "eCommerce platform to manage and market your online store.",
+        "services": "Zoho Commerce is a comprehensive eCommerce platform that's perfect for managing and marketing your online store. It equips businesses of all sizes to establish, launch, and expand their online sales channels.This platform offers features like inventory management, secure payment options, and a range of marketing tools to simplify eCommerce complexities. Whether you're just starting or looking to scale your online store, Zoho Commerce provides the tools and support you need to succeed.If you're ready to bring your products and services online, Zoho Commerce is your trusted partner. Try it now and build a robust online store that drives growth and customer satisfaction.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/N1wQmzS/Marketing-Automation.png",
         "name": "Marketing Automation",
         "description": "An all-in-one marketing automation software.",
+        "services": "Zoho Marketing Automation is the all-in-one marketing automation software that can transform your marketing strategies. From lead generation to customer engagement and retention, this platform streamlines your marketing effortsWith Zoho Marketing Automation, you can create personalized customer journeys, automate repetitive tasks, and analyze campaign results to refine your approach. Whether you're a small business or a large enterprise, this tool offers the features you need to drive results and enhance customer experiences.If you're looking to elevate your marketing game and maximize the impact of your campaigns, try Zoho Marketing Automation now.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/PCrJKCC/writer.png",
         "name": "LandingPage",
         "description": "Smart landing page builder to increase conversion rates.",
+        "services": "Zoho LandingPage is the smart choice for building high-converting landing pages without the need for coding. Whether you're a business or a marketer, this platform empowers you to create visually appealing and effective landing pagesWith an intuitive drag-and-drop editor and optimization tools, Zoho LandingPage ensures your landing pages capture leads and drive conversions. If you're aiming to create landing pages that convert visitors into customers, try it now and see the difference.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/MBnt99K/SalesIQ.jpg",
         "name": "SalesIQ",
         "description": "Live-chat app to engage and convert website visitors.",
+        "services": "Zoho SalesIQ is the live-chat app that can transform your website into a powerful sales and support tool. It enables you to engage and convert website visitors in real-time. With visitor tracking, chatbots, and analytics, SalesIQ empowers your team to offer exceptional customer support and drive conversions. Whether you run an e-commerce store or a service-based business, this tool can enhance your online presence and boost your bottom line. Try Zoho SalesIQ now and turn website visitors into loyal customers.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/prZkw0Q/Sign.jpg",
         "name": "Sign",
         "description": "Digital signature app for businesses.",
+        "services": "Zoho Sign is the digital signature app tailored to businesses. It simplifies the process of signing documents electronically, eliminating the need for manual signatures and paperwork.With Zoho Sign, you can send documents for signature, track their status, and maintain a digital record of all transactions securely and efficiently. If you're looking to streamline your document signing process and reduce paperwork, Zoho Sign is your solution. Try it now and experience the convenience of digital signatures.",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/0Ffm8w1/Thrive.png",
         "name": "Thrive",
         "description": "A complete loyalty and affiliate management platform.",
+        "services": "Zoho Thrive is your complete loyalty and affiliate management platform. It's the key to rewarding and incentivizing your customers, partners, and affiliates. With customizable loyalty programs, affiliate tracking, and performance analytics, Zoho Thrive helps you build lasting relationships and grow your customer base. Whether you're a small business or a large enterprise, this platform provides the tools you need to boost customer retention and expand your network of affiliates",
         "btn": "TRY NOW"
     },
     {
         "image": "https://i.ibb.co/2nknWZ0/Voice.png",
         "name": "Voice",
         "description": "Cloud contact center software for businesses.",
+        "services": "Zoho Voice is your cloud contact center software designed for businesses. It enables you to provide exceptional customer support and engagement through voice communications.With features like call routing, analytics, and integration capabilities, Zoho Voice empowers your team to deliver top-notch customer experiences. Try Zoho Voice now to take your customer support to the cloud.These blog posts offer concise descriptions of each service without images. You can use these descriptions to showcase your services on your website or marketing materials. If you have any specific details or updates to add, feel free to let me know, and I can assist you further.",
         "btn": "TRY NOW"
     }
 
