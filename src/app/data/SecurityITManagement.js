eb702f50c9baa139ac5a042780f3b5da107a4e27
rezaul118255 <rezaul118255@gmail.com>
2023-09-04 15:16:15 +0600

    compleate Json data

--- src/app/data/SecurityITManagement.js
+++ src/app/data/SecurityITManagement.js
@@ -1,117 +1,132 @@
 export const UserData = [
-    {
-      "image": "https://i.ibb.co/L6CCD5g/images-q-tbn-ANd9-Gc-Q-F4jib-W-Pt-PMz3c-N27y-P07p-Ai-Dp-Uj-N6j4-w-usqp-CAU.png",
-      "name": "Directory",
-      "description": "Workforce identity and access management solution for cloud businesses.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/T4PTYwz/vault.png",
-      "name": "Vault",
-      "description": "Online password manager for teams.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/T4PTYwz/vault.png",
-      "name": "Catalyst",
-      "description": "Build and deploy serverless apps.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/wRLmKQq/toolkit.png",
-      "name": "Toolkit",
-      "description": "A complete resource for any admin-related lookup queries.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/tQSFxrc/lens.png",
-      "name": "Lens",
-      "description": "Interactive remote assistance software with augmented reality.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/bBq4DK8/sprint.png",
-      "name": "An all-in-one monitoring solution",
-      "description": "for DevOps and IT Operations. TRY NOW",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/YTKfQ8R/customer-care.png",
-      "name": "ServiceDesk Plus",
-      "description": "Cloud-based IT ticketing system to resolve issues faster.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/1KxsN3m/people.png",
-      "name": "Mobile Device Management",
-      "description": "Set up and secure your enterprise mobile devices and apps.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/19MB9yC/commerce.png",
-      "name": "Patch Manager Plus",
-      "description": "Stay clear of vulnerabilities and cyber attacks.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/fQX3pHN/inventory.png",
-      "name": "Identity Management",
-      "description": "A secure, single sign-on solution for enterprises.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/2KCMvTf/cloud.png",
-      "name": "Log360 Cloud",
-      "description": "Store and manage your network logs with a cloud SIEM solution.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/mHSC7BZ/assist.png",
-      "name": "Assist",
-      "description": "Remote support and unattended remote access software.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/G7Xj04R/remote.png",
-      "name": "Remote Access Plus",
-      "description": "Secure and comprehensive unattended access at the click of your mouse.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/X2GygQG/endpoint.png",
-      "name": "Endpoint Central",
-      "description": "A 360-degree UEM solution for endpoint management and security.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
-    {
-      "image": "https://i.ibb.co/8mWT32b/statusiq.png",
-      "name": "StatusIQ",
-      "description": "Status page for real-time status and incident communication.",
-      "btn-route": "/route-here",
-      "btn": "TRY NOW"
-    },
+  {
+    "image": "https://i.ibb.co/L6CCD5g/images-q-tbn-ANd9-Gc-Q-F4jib-W-Pt-PMz3c-N27y-P07p-Ai-Dp-Uj-N6j4-w-usqp-CAU.png",
+    "name": "Directory",
+    "description": "Workforce identity and access management solution for cloud businesses.",
+    "services": "Directory is an essential workforce identity and access management solution tailored for cloud-based businesses. It offers robust user identity management and access control, ensuring data security and compliance. With Directory, you can streamline user provisioning, access policies, and authentication processes, enhancing your organization's cybersecurity posture and overall operational efficiency.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/T4PTYwz/vault.png",
+    "name": "Vault",
+    "description": "Online password manager for teams.",
+    "services": "Vault is a user-friendly online password manager designed for teams. It simplifies password management, making it effortless to securely store and share passwords among team members. Vault's encryption and access controls ensure that sensitive information remains protected while enabling seamless collaboration within your organization.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/T4PTYwz/vault.png",
+    "name": "Catalyst",
+    "description": "Build and deploy serverless apps.",
+    "services": "Catalyst is your go-to platform for building and deploying serverless applications quickly and efficiently. Whether you're a developer or a business owner, Catalyst's intuitive tools and pre-configured templates streamline the creation of scalable and cost-effective serverless apps, accelerating your development cycles and reducing operational overhead.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/wRLmKQq/toolkit.png",
+    "name": "Toolkit",
+    "description": "A complete resource for any admin-related lookup queries.",
+    "services": "Toolkit serves as a comprehensive resource for administrative tasks and lookups. From troubleshooting to resource retrieval, Toolkit equips administrators with a suite of powerful tools to maintain system health, troubleshoot issues, and access essential information swiftly, all from a single, user-friendly interface.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/tQSFxrc/lens.png",
+    "name": "Lens",
+    "description": "Interactive remote assistance software with augmented reality.",
+    "services": "Lens offers interactive remote assistance with augmented reality capabilities. This innovative software empowers technical support teams to provide real-time guidance and..Lens offers interactive remote assistance with augmented reality capabilities. This innovative software empowers technical support teams to provide real-time guidance and..Lens offers interactive remote assistance with augmented reality capabilities. This innovative software empowers technical support teams to provide real-time guidance and",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/bBq4DK8/sprint.png",
+    "name": "An all-in-one monitoring solution",
+    "description": "for DevOps and IT Operations. TRY NOW",
+    "services": "This service provides a comprehensive monitoring solution for DevOps and IT Operations. With its range of monitoring and analytics tools, it enables organizations to proactively identify and address performance and security issues, ensuring the smooth operation of critical systems and applications. Try it now to optimize your IT infrastructure's performance and security.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/YTKfQ8R/customer-care.png",
+    "name": "ServiceDesk Plus",
+    "description": "Cloud-based IT ticketing system to resolve issues faster.",
+    "services": "ServiceDesk Plus is a cloud-based IT ticketing system designed to streamline issue resolution processes. It empowers IT teams to efficiently track, manage, and resolve support tickets, enhancing overall service delivery. With its user-friendly interface and automation capabilities, ServiceDesk Plus accelerates incident resolution, minimizes downtime, and improves customer support.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/1KxsN3m/people.png",
+    "name": "Mobile Device Management",
+    "description": "Set up and secure your enterprise mobile devices and apps.",
+    "services": "Secure and manage enterprise mobile devices and applications with Mobile Device Management. It provides robust tools for device provisioning, security policy enforcement, and remote management, ensuring that your mobile infrastructure remains protected and compliant.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/19MB9yC/commerce.png",
+    "name": "Patch Manager Plus",
+    "description": "Stay clear of vulnerabilities and cyber attacks.",
+    "services": "Stay ahead of vulnerabilities and cyber threats with Patch Manager Plus. This service automates the patch management process, ensuring that your software and systems are up to date and protected against security risks, reducing the likelihood of cyberattacks.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/fQX3pHN/inventory.png",
+    "name": "Identity Management",
+    "description": "A secure, single sign-on solution for enterprises.",
+    "services": "Identity Management offers a secure single sign-on solution for enterprises. Simplify user authentication and access control while maintaining security and compliance. Centralize identity management and protect sensitive data with ease.Identity Management offers a secure single sign-on solution for enterprises. Simplify user authentication and access control while maintaining security and compliance. Centralize identity management and protect sensitive data with ease.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/2KCMvTf/cloud.png",
+    "name": "Log360 Cloud",
+    "description": "Store and manage your network logs with a cloud SIEM solution.",
+    "services": "Store and manage your network logs with a cloud SIEM (Security Information and Event Management) solution. Log360 Cloud provides real-time monitoring, threat detection, and compliance reporting, helping you secure your network infrastructure.Store and manage your network logs with a cloud SIEM (Security Information and Event Management) solution. Log360 Cloud provides real-time monitoring, threat detection, and compliance reporting, helping you secure your network infrastructure.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/mHSC7BZ/assist.png",
+    "name": "Assist",
+    "description": "Remote support and unattended remote access software.",
+    "services": "Assist is a versatile remote support and unattended remote access software. It enables IT teams and support professionals to provide efficient remote assistance to users, improving issue resolution times and reducing the need for physical on-site support.Assist is a versatile remote support and unattended remote access software. It enables IT teams and support professionals to provide efficient remote assistance to users, improving issue resolution times and reducing the need for physical on-site support.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/G7Xj04R/remote.png",
+    "name": "Remote Access Plus",
+    "description": "Secure and comprehensive unattended access at the click of your mouse.",
+    "services": "Secure and comprehensive unattended access at your fingertips. With Remote Access Plus, you can effortlessly access and manage remote systems, enhancing productivity and troubleshooting capabilitiesSecure and comprehensive unattended access at your fingertips. With Remote Access Plus, you can effortlessly access and manage remote systems, enhancing productivity and troubleshooting capabilities",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/X2GygQG/endpoint.png",
+    "name": "Endpoint Central",
+    "description": "A 360-degree UEM solution for endpoint management and security.",
+    "services": "Endpoint Central offers a 360-degree Unified Endpoint Management (UEM) solution for endpoint management and security. It helps organizations efficiently manage and secure a diverse range of endpoints, ensuring data protection and compliance.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
+  {
+    "image": "https://i.ibb.co/8mWT32b/statusiq.png",
+    "name": "StatusIQ",
+    "description": "Status page for real-time status and incident communication.",
+    "services": "StatusIQ provides a real-time status and incident communication platform. Create status pages to keep stakeholders informed about service availability and incidents, improving transparency and communication during downtime or disruptions.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  },
 
-    {
-        "image":"https://i.ibb.co/Fbh5N7S/creator.png",
-        "name": "Creator",
-        "description": "Build customized apps to simplify business processes.",
-        "btn-route": "/route-here",
-        "btn": "TRY NOW"
-    }
+  {
+    "image": "https://i.ibb.co/Fbh5N7S/creator.png",
+    "name": "Creator",
+    "description": "Build customized apps to simplify business processes.",
+    "services": " Creator empowers you to build customized apps that simplify your business processes. Tailor applications to your unique needs without extensive coding, streamlining workflows and boosting efficiency. Try Creator now to unlock your business's full potential through customized software solutions.",
+    "btn-route": "/route-here",
+    "btn": "TRY NOW"
+  }
 
-  ]
-  
+]
