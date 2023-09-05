dda12c0734560960b7de9ab9fdb8ee9f6015b099
anayet ullah <anayetdev@gmail.com>
2023-09-05 08:38:18 +0600

    Db connect error solved

--- src/services/DbConnect.js
+++ src/services/DbConnect.js
@@ -1,9 +1,8 @@
+
+
 export const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4hmio3i.mongodb.net/Infonexus?retryWrites=true&w=majority`;
+
 
 //use only test
 export const apiTest = `mongodb+srv://${process.env.DB_USER2}:${process.env.DB_PASS2}@cluster0.qthn2pl.mongodb.net/bistroDB?retryWrites=true&w=majority`;
-<<<<<<< HEAD
-console.log(apiTest);
-=======
 // console.log(apiTest)
->>>>>>> 80c84f9af20d3267d3ba629047d2cc7695a47f2b
